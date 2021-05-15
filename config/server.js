b1488fbf06bb3fc91ccfe0c946227c4d5d577dd0
aegirorn <41763474+aegirorn@users.noreply.github.com>
2021-05-15 15:23:59 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd39452d9858ca859ea869d28288b438'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '861eaa3e-5a04-48ce-8af5-36974de5edcb'
+};
