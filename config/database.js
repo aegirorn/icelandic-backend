4a9364d3d103552b7dc9e4c6474b67731d71bb4f
aegirorn <41763474+aegirorn@users.noreply.github.com>
2021-05-15 21:17:29 +0000

    Fixed a bug in database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,5 @@
-const parse = require("pg-connection-string").parse;
-
 module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-
     return {
       defaultConnection: "default",
       connections: {
@@ -15,41 +11,6 @@ module.exports = ({ env }) => {
           options: {
             ssl: true,
           },
-        },
-      },
-    };
-  } else if (env("NODE_ENV") === "devSqlLite") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "sqlite",
-            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
-      },
-    };
-  } else if (env("NODE_ENV") === "pg") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: env("DATABASE_HOST", "127.0.0.1"),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "strapi-icelandic"),
-            username: env("DATABASE_USERNAME", "app-user"),
-            password: env("DATABASE_PASSWORD", "strumpur"),
-            ssl: env.bool("DATABASE_SSL", false),
-          },
-          options: {},
         },
       },
     };
