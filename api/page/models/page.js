89e01ce467f85069d5af14fa83f9d351096be72d
aegirorn <aegir@nordural.is>
2021-06-04 07:43:08 +0000

    Modified for next.js

--- api/page/models/page.js
+++ api/page/models/page.js
@@ -1,8 +1,32 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      var lesson = await strapi.query("lesson").findOne({ id: data.lesson });
 
-module.exports = {};
+      if (lesson && data.lesson && data.pageNo) {
+        data.slug = `${lesson.key}-${data.pageNo}`;
+      }
+
+      if (data.slug) {
+        var res = data.slug.split("-");
+        data.levelNo = res[0];
+        data.lessonNo = res[1];
+      }
+    },
+    async beforeUpdate(params, data) {
+      var lesson = await strapi.query("lesson").findOne({ id: data.lesson });
+
+      if (lesson && data.lesson && data.pageNo) {
+        data.slug = `${lesson.key}-${data.pageNo}`;
+      }
+
+      if (data.slug) {
+        var res = data.slug.split("-");
+        data.levelNo = res[0];
+        data.lessonNo = res[1];
+      }
+    },
+  },
+};
